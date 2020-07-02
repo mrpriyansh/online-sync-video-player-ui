af4327d2bfe9d3ee1e45b15f8935bf5e6790e6e6
Anuj Singh <anujs787@gmail.com>
2020-07-02 23:32:20 +0530

    Added Footer and Navbar

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import styles from './Footer.module.css'
+
+const Footer = props => (
+    <div className={styles.footer}>
+        <div className={styles.container}>
+            <div className={styles.row}>
+                <div className={styles.column}>
+                    <h4>AOISJDOIAJSDO</h4>
+                    <ul className={styles.list}>
+                        <li>474015</li>
+                        <li>Atal bihari vajpayee</li>
+                        <li>Morena link road</li>
+                    </ul>
+                </div>
+                <div className={styles.column}>
+                    <h4>AOISJDOIAJSDO</h4>
+                    <ul className={styles.list}>
+                        <li>474015</li>
+                        <li>Atal bihari vajpayee</li>
+                        <li>Morena link road</li>
+                    </ul>
+                </div>
+                <div className={styles.column}>
+                    <h4>AOISJDOIAJSDO</h4>
+                    <ul className={styles.list}>
+                        <li>474015</li>
+                        <li>Atal bihari vajpayee</li>
+                        <li>Morena link road</li>
+                    </ul>
+                </div>
+            </div>
+            <hr></hr>
+            <div className={styles.bottom}>
+                &copy;{new Date().getFullYear()} DEV-PROJECT | All Rights Reserved | Terms of Service | Privacy
+            </div>
+        </div>
+    </div>
+)
+
+export default Footer;
