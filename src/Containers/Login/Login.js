63a25137725419911bfddd11272b627e1e70a3db
GokulRaj-R <gokul8262@gmail.com>
2020-07-06 23:58:52 +0530

    Added login form methods

--- src/Containers/Login/Login.js
+++ src/Containers/Login/Login.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import styles from '../Form.module.css';
 
 const Login = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => {
+    fetch('https://localhost:3000/login', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    });
+  };
+
   return (
     <div className={styles.box}>
-      <form className={styles.form}>
+      <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
         <h2 className={styles.h2}>Login</h2>
-        <input className={styles.input} id="email" type="email" placeholder="Email" />
+        <input
+          className={styles.input}
+          name="email"
+          type="email"
+          placeholder="Email"
+          ref={register}
+          required
+        />
         <br />
-        <input className={styles.input} id="password" type="password" placeholder="Password" />
+        <input
+          className={styles.input}
+          name="password"
+          type="password"
+          placeholder="Password"
+          ref={register}
+          required
+        />
         <br />
-        <input className={styles.btn} type="submit" value="Log In" />
+        <input className={styles.btn} type="submit" value="login" />
         <p className={styles.p}>
           Not Registered? <Link to="/register">Create an account</Link>
         </p>
