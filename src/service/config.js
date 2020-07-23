2d502c805e62bae3b8c468768209856c4d6aba65
Gaharana <mr.priyanshgaharana@gmail.com>
2020-07-23 22:28:33 +0530

    Also Integrates with Hosted API

--- src/service/config.js
+++ src/service/config.js
@@ -1,4 +1,8 @@
-const apiUrl = 'http://localhost:4000';
+// const apiUrl = 'http://localhost:4000';
+const apiUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'https://dev-project-backend.vercel.app'
+    : 'http://localhost:4000';
 
 // eslint-disable-next-line import/prefer-default-export
 export { apiUrl };
