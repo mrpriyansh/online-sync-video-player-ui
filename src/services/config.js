de6c26832e1c36558f38df75da226a6ff14b6db1
Priyansh Gaharana <34992117+mrpriyansh@users.noreply.github.com>
2021-06-18 13:02:06 +0530

    Update config.js

--- src/services/config.js
+++ src/services/config.js
@@ -1,7 +1,7 @@
 // const apiUrl = 'http://localhost:4000';
 // const apiUrl = 'https://dev-project-backend.herokuapp.com';
 const apiUrl =
-  process.env.NODE_ENV !== 'production'
+  process.env.NODE_ENV === 'production'
     ? 'https://dev-project-backend.herokuapp.com'
     : 'http://localhost:4000';
 
